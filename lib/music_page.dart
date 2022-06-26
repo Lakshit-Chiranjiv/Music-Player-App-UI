7c21a4568a605a499b9770805672d574a29d17db
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-26 14:08:24 +0530

    created the neumorphic container

--- lib/music_page.dart
+++ lib/music_page.dart
@@ -10,6 +10,30 @@ class MusicPage extends StatefulWidget {
 class _MusicPageState extends State<MusicPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue[300],
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 300,
+          decoration: BoxDecoration(
+            color: Colors.blue[300],
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.blue.shade500,
+                blurRadius: 15,
+                offset: const Offset(5, 5)
+              ),
+              const BoxShadow(
+                color: Colors.white60,
+                blurRadius: 15,
+                offset: Offset(-5, -5)
+              )
+            ]
+          ),
+        ),
+      ),
+    );
   }
 }
