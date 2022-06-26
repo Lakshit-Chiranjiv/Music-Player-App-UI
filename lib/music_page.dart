b9c4b665f52b9b4dc2c426854be3e7669f164e21
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-06-26 14:11:10 +0530

    imported neu container

--- lib/music_page.dart
+++ lib/music_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/neu_container.dart';
 
 class MusicPage extends StatefulWidget {
   MusicPage({Key? key}) : super(key: key);
@@ -13,26 +14,7 @@ class _MusicPageState extends State<MusicPage> {
     return Scaffold(
       backgroundColor: Colors.blue[300],
       body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          decoration: BoxDecoration(
-            color: Colors.blue[300],
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.shade500,
-                blurRadius: 15,
-                offset: const Offset(5, 5)
-              ),
-              const BoxShadow(
-                color: Colors.white60,
-                blurRadius: 15,
-                offset: Offset(-5, -5)
-              )
-            ]
-          ),
-        ),
+        child: NeuContainer()
       ),
     );
   }
